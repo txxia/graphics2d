8d17ef37f78c2f75d62f701ffb5fc99f05dc15f2
Tian Xia <cruisky@gmail.com>
2014-08-15 19:30:21 -0400

    modularize code

--- 2.2_dda/main.c
+++ 2.2_dda/main.c
@@ -2,12 +2,13 @@
 #include "raster_scan.h"
 #define WIDTH 640
 #define HEIGHT 480
+#define DELAY 8
 
-void draw(SDL_Renderer *renderer, int x1, int y1, int x2, int y2){
-    // draw a referencing line
-    drawLine(renderer,
-             toPixel(x1), toPixel(y1), toPixel(x2), toPixel(y2));
+SDL_Renderer *pRenderer;
 
+/* Digital Differential Analyzer routine for rastering a line*/
+void DDA(int x1, int y1, int x2, int y2,
+         void (*callback)(int, int)){
     // approximate the line length
     int length = (abs(x2-x1) >= abs(y2-y1)) ? abs(x2-x1) : abs(y2-y1);
 
@@ -22,12 +23,22 @@ void draw(SDL_Renderer *renderer, int x1, int y1, int x2, int y2){
 
     // begin main loop
     for(int i=0; i<=length; i++){
-        setPixel(renderer, (int)x, (int)y, parseColor("0x0000ff11"));
+        callback((int)x, (int)y);
         x += dx;
         y += dy;
-        SDL_Delay(10);
     }
+}
 
+void setPix(int x, int y){
+    setPixel(pRenderer, x, y, parseColor("#000000"));
+    SDL_Delay(DELAY);
+}
+
+void draw(){
+    // draw a referencing line
+    drawLine(pRenderer,
+             toPixel(80), toPixel(60), toPixel(4), toPixel(7));
+    DDA(80, 60, 4, 7, setPix);
 }
 
 int main(int argc, char *argv[]) {
@@ -42,7 +53,7 @@ int main(int argc, char *argv[]) {
                                           SDL_WINDOW_SHOWN);
 
     // Setup renderer
-    SDL_Renderer* pRenderer =  SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED);
+    pRenderer =  SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED);
 
     // Set render color to red.
     SDL_SetRenderDrawColor(pRenderer, 255, 255, 255, 255);
@@ -54,7 +65,7 @@ int main(int argc, char *argv[]) {
     drawGrid(pRenderer, WIDTH, HEIGHT);
 
     // default drawing function
-    draw(pRenderer, 80, 60, 4, 7);
+    draw();
 
     // The window is open: enter program loop
     while(loop) {
